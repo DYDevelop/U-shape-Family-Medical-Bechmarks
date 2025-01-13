7c2b51de270123cac55d58b84386a15675d56cdb
Kim Dong Yeong <55197580+DYDevelop@users.noreply.github.com>
2025-01-13 15:45:34 +0900

    Add files via upload

--- infer.py
+++ infer.py
@@ -7,7 +7,7 @@ from src.dataloader.dataset import MedicalDataSets
 from albumentations import Compose, Resize, Normalize
 from albumentations.pytorch import ToTensorV2
 import src.utils.losses as losses
-from src.utils.metrics import iou_score
+from src.utils.metrics import iou_score, multiclass_metrics
 from torchvision.utils import save_image
 # Assuming model imports based on your provided training script
 from src.network.conv_based.CMUNet import CMUNet
@@ -79,15 +79,11 @@ def get_val_transform(img_size):
         # ToTensorV2(),
     ])
 
-def validate(model, val_loader, criterion, device, save_dir="validation_results"):
-    """执行验证，并且每隔十张图像保存一次预测结果到PNG文件"""
+def validate(model, val_loader, criterion, device, save_dir="validation_results", num_classes=1):
     model.eval()
     val_loss = 0.0
-    val_iou = 0.0
-    val_dice = 0.0
-    val_rvd = 0.0
-    os.makedirs(save_dir, exist_ok=True)  
-
+    os.makedirs(save_dir, exist_ok=True)
+    fold_metrics  = [{'iou':[], 'dsc':[], 'sensitivity':[], 'specificity':[], 'precision':[], 'accuracy':[], 'f1_score':[]} for _ in range(num_classes)]
     with torch.no_grad():
         for i_batch, sampled_batch in enumerate(val_loader):
             img_batch, label_batch = sampled_batch['image'], sampled_batch['label']
@@ -97,12 +93,7 @@ def validate(model, val_loader, criterion, device, save_dir="validation_results"
 
             val_loss += loss.item()
 
-            iou, dice, rvd, _, _, _, _, _ = iou_score(outputs, label_batch)
-            val_iou += iou
-            val_dice += dice
-            if rvd<1:
-                val_rvd += rvd
-            # 每隔十张图像保存一次预测结果
+            iter_eval = multiclass_metrics(outputs, label_batch)
             if i_batch % 1 == 0:
                 # 将模型输出转换为二值图像
                 outputs = torch.sigmoid(outputs)
@@ -116,11 +107,17 @@ def validate(model, val_loader, criterion, device, save_dir="validation_results"
                     # 使用save_image从torchvision，或者使用其他方法将张量转换为图像并保存
                     save_image(img, save_path)
 
-    val_loss /= len(val_loader)
-    val_iou /= len(val_loader)
-    val_dice /= len(val_loader)
-    val_rvd /= len(val_loader)
-    print(f'验证损失: {val_loss:.4f}, 验证IoU: {val_iou:.4f}, 验证dice：{val_dice:.4f}, 验证rvd：{val_rvd:.4f}')
+            for classes in range(num_classes):
+                for key in fold_metrics[classes].keys():fold_metrics[classes][key].extend(iter_eval[classes][key])
+
+    for classes in range(num_classes):
+        for key in fold_metrics[classes].keys(): 
+            fold_metrics[classes][key] = sum(fold_metrics[classes][key]) / len(fold_metrics[classes][key]) if fold_metrics[classes][key] else 0.0
+    for classes in range(num_classes):
+        print(f'-- Currnet fold Class {classes} evaluation metrics --')
+        for key, values in fold_metrics[classes].items(): print(f'{key}: {values:.3f}')
+
+    return fold_metrics
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Validation script for medical image segmentation")
@@ -131,15 +128,30 @@ if __name__ == "__main__":
     parser.add_argument('--img_size', type=int, default=256, help='image size')
     parser.add_argument('--num_classes', type=int, default=1, help='number of classes')
     parser.add_argument('--batch_size', type=int, default=1, help='batch size')
+    parser.add_argument('--k_fold', type=int, default=5, help='number of folds')
     args = parser.parse_args()
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = load_model(args.model_path, args, device)
 
-    val_transform = get_val_transform(args.img_size)
+    total_metrics  = [{'iou':[], 'dsc':[], 'sensitivity':[], 'specificity':[], 'precision':[], 'accuracy':[], 'f1_score':[]} for _ in range(args.num_classes)]
 
-    db_val = MedicalDataSets(base_dir=args.base_dir, split="val", transform=val_transform, val_file_dir=args.val_file_dir)
-    val_loader = DataLoader(db_val, batch_size=args.batch_size, shuffle=False, num_workers=4, pin_memory=True)
+    for fold in range(args.k_fold):
+        model = load_model(args.model_path+args.model+f'_{fold}_model.pth', args, device)
 
-    criterion = losses.__dict__['BCEDiceLoss']().to(device)
-    validate(model, val_loader, criterion, device)
+        val_transform = get_val_transform(args.img_size)
+
+        db_val = MedicalDataSets(base_dir=args.base_dir, split="val", transform=val_transform, val_file_dir=args.val_file_dir, n_classes=args.num_classes)
+        val_loader = DataLoader(db_val, batch_size=args.batch_size, shuffle=False, num_workers=4, pin_memory=True)
+
+        criterion = losses.__dict__['BCEDiceLoss']().to(device)
+        fold_metrics = validate(model, val_loader, criterion, device, num_classes=args.num_classes)
+
+        for classes in range(args.num_classes):
+            for key in total_metrics[classes].keys():total_metrics[classes][key].append(fold_metrics[classes][key])
+
+    for classes in range(args.num_classes):
+        print(f'-- Final Class {classes} evaluation metrics --')
+        for key in total_metrics[classes].keys(): 
+            print(f"{key} : {np.mean(total_metrics[classes][key]):.3f} ± {np.std(total_metrics[classes][key]):.3f}")
+
+# python infer.py --model U_Net --model_path checkpoints/ --base_dir ../data/axi --val_file_dir axi_val.txt --img_size 256 --num_classes 2
